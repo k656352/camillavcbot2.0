a0d167ca8b9704e7ca8889ee81432fa6492337db
k656352 <91837555+k656352@users.noreply.github.com>
2021-10-16 08:04:44 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -118,7 +118,7 @@ async def play(_, message: Message):
                 try:
                     await USER.join_chat(invitelink)
                     await USER.send_message(
-                        message.chat.id, "**camila Music assistant joined this group for play music 🎵**")
+                        message.chat.id, "**🎵 Amanda Music assistant joined this group for play music 🎵**")
 
                 except UserAlreadyParticipant:
                     pass
